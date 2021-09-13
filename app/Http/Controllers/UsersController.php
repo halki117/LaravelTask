f42e1ffbae424465013c594c7f75b87953d8c5d1
halki <s.halki0203@icloud.com>
2021-09-13 15:31:32 +0900

    view修正

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,7 +39,7 @@ class UsersController extends Controller
     }
 
     public function session() {
-        return view('users.session');
+        return view('users.session');        
     }
 
     public function login(Request $request) {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,24 @@
 @extends('app')
 
+@section('header')
+    <div class="header">
+      @parent
+
+      <div class="navlink">
+        @if ($current_user['name'])
+          <div class="current_user_name">
+            <p>{{ $current_user['name'] }}</p>
+          </div>
+          <div class="logout">
+            <a href="#"><p>ログアウト</p></a>
+          </div>
+        @endif
+      </div>
+    </div>
+@endsection
+
+
 @section('content')
-  
-  @if ($current_user['name'])
-      <p>{{ $current_user['name'] }}</p>
-  @endif
 
   <div class="loggedin_message">
     <h1>You are logged in!!</h1>
